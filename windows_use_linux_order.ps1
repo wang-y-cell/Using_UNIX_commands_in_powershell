7c172baf0d928716cda438f158b84a1f4b4483ea
wang_y <3084003139@qq.com>
2026-07-26 11:34:14 +0800

    修改终端提示符

--- windows_use_linux_order.ps1
+++ windows_use_linux_order.ps1
@@ -59,7 +59,7 @@ function prompt {
     $path = $ExecutionContext.SessionState.Path.CurrentLocation
     
     # 用户名@主机名 (绿色)
-    Write-Host "wangy@windowsPS:" -ForegroundColor Green -NoNewline
+    Write-Host "windows@PS:" -ForegroundColor Green -NoNewline
     
     # 路径 (深蓝)
     Write-RGB -Text "$path" -R 97 -G 175 -B 239 -NoNewline
